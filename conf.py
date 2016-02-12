e221ec850d90264e4f52a4c20d1ab8995a98f4fc
Mirjam Baltus <m.baltus@furore.com>
2016-02-12 13:05:08 +0100

    x

--- conf.py
+++ conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 # Configuration for intersphinx
-intersphinx_mapping = {'testingdocs': ('projects/testingmydocs/en/latest', None)}
+intersphinx_mapping = {'testingdocs': ('projects/testingmydocs/en/latest/_build', None)}
 
 
 # Add any paths that contain templates here, relative to this directory.
